05eb81ff2283a9732d0c30a0e44af63fd6042fc7
Jonathan Yu <4723726+jyucsiro@users.noreply.github.com>
2018-02-20 12:11:15 +1100

    enabling fanstatic and starting to dev template func

--- ckanext/digitalassetfields/plugin.py
+++ ckanext/digitalassetfields/plugin.py
@@ -3,6 +3,20 @@
 import ckan.plugins as p
 import ckan.plugins.toolkit as tk
 
+def most_popular_groups():
+    '''Return a sorted list of the groups with the most datasets.'''
+
+    # Get a list of all the site's groups from CKAN, sorted by number of
+    # datasets.
+    groups = toolkit.get_action('group_list')(
+        data_dict={'sort': 'package_count desc', 'all_fields': True})
+
+    # Truncate the list to the 10 most popular groups only.
+    groups = groups[:10]
+
+    return groups
+
+
 
 class DigitalassetfieldsPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
    p.implements(p.IDatasetForm)
@@ -81,3 +95,5 @@ class DigitalassetfieldsPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
         # Add this plugin's templates dir to CKAN's extra_template_paths, so
         # that CKAN will use this plugin's custom templates.
         tk.add_template_directory(config, 'templates')
+        tk.add_resource('fanstatic', 'digitalassetfields')
+
