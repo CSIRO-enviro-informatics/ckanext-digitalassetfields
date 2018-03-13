c99389b46e0c2cb2bed3e36ffcce075e88e29c8d
Jonathan Yu <jonathan.yu@csiro.au>
2018-03-13 09:02:22 +0000

    add pub lookup

--- ckanext/digitalassetfields/fanstatic/digitalassetfields_dataset_form.js
+++ ckanext/digitalassetfields/fanstatic/digitalassetfields_dataset_form.js
@@ -22,3 +22,22 @@ ckan.module('digitalassetfields_project_select', function ($) {
   };
 });
 
+ckan.module('digitalassetfields_publication_select', function ($) {
+  return {
+    initialize: function () {
+      console.log("digitalassetfields_publication_select - I've been initialized for element: ", this.el);
+
+      this.el.change(function() {
+         console.log( "Handler for .change() called. ", $(this) );
+
+         var url = "http://network.csiro.au:9500/standalone/publications.json?title=" + $(this).val();
+         console.log( "url: ", url);
+         $.get( url, function( data ) {
+            console.log( 'Querying network.csiro.au');
+            console.log( data);
+         });
+      });
+    }
+  };
+});
+
