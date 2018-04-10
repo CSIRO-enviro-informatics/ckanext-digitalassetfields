1a8b3d3f313b0fa2e35d271c35675e55bee33e6f
Jonathan Yu <jonathan.yu@csiro.au>
2018-04-10 00:42:38 +0000

    improving usability of tooltips

--- ckanext/digitalassetfields/fanstatic/digitalassetfields_tooltips.js
+++ ckanext/digitalassetfields/fanstatic/digitalassetfields_tooltips.js
@@ -5,17 +5,37 @@ ckan.module('digitalassetfields_tooltips', function ($) {
     initialize: function () {
       console.log("I've been initialized for element: ", this.el);
       //find the label associated
+      const currEl = $(this.el);
       const cg = $(this.el).closest('div.control-group');
       console.log(cg);
       const cgLabel = cg.find('label.control-label');
       console.log(cgLabel);
 
+      console.log("this");
+      console.log(this);
+      console.log("this.options");
+      console.log(this.options);
+
       if(this.options.hasOwnProperty('tooltip')) {
          console.log('Tooltip: ' + this.options.tooltip);
       }
 
-      cgLabel.popover({title: 'Info', html: true,
-                       content: this._(this.options.tooltip), trigger: 'hover', placement: 'left'});
+
+      if(this.options.hasOwnProperty('add_help_icon')) {
+         console.log('Add help icon: ' + this.options.add_help_icon);
+ 
+      }
+
+
+      const helpIcon = $('<a><i class="fa fa-question-circle"></i></a>')
+      const title = cgLabel.text();
+      const content = "<p>" + cgLabel.text() + "</p>";
+      helpIcon.popover({title: title, html: true,
+                       content: this._(this.options.tooltip), trigger: 'hover', delay: {"hide": 500}, placement: 'left'});
+
+      cgLabel.append(" ", helpIcon);
+      //cgLabel.popover({title: 'Info', html: true,
+      //                 content: this._(this.options.tooltip), trigger: 'focus click hover', placement: 'left'});
 
     }
   };
